e5d0e6a44743d9e42b487851b747c14f8fae7459
Clove Twilight <clovetwilight3@outlook.com>
2026-04-19 17:16:40 +0100

    Add command for support, pointing to the Discord server, bot status and source code

---
+++ src/commands/support.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2026 Aria Rees & Clove Nytrix Doughmination Twilight
+ * Licensed under the MIT Licence.
+ * See LICENCE in the project root for full licence information.
+ */
+
+import { 
+    CommandInteraction, 
+    EmbedBuilder,
+    MessageFlags
+} from 'discord.js';
+
+export default {
+    data: {
+        name: 'support',
+        description: 'Get the support links',
+    },
+
+    async execute(interaction: CommandInteraction, client: any) {
+        if (!interaction.isChatInputCommand()) return;
+
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+
+        const sent = await interaction.editReply({ content: 'Please wait...' });
+
+        const embed = new EmbedBuilder()
+            .setTitle('🎗️ Support Links')
+            .setDescription(
+                `**Support Server:**https://discord.gg/transfemme
+                \n**Source Code:** https://github.com/Girls-Network/GayBot-v2
+                \n**Bot Status:** https://status.girlnetwork.dev`
+            )
+            .setColor(0x2ecc71);
+
+        await interaction.editReply({ content: '', embeds: [embed] });
+    },
+};
